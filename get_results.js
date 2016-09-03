2b1e17e0cd4045119f8c48cea0f994e9d953ba3d
jodevsa <jodevsa@gmail.com>
2016-09-04 02:32:49 +0300

    added live results emiiter

--- get_results.js
+++ get_results.js
@@ -1,59 +1,67 @@
 const request = require('request');
 const options = require('./http_options');
 const WebSocket = require('ws');
-function get_results(eventEmitter){
+var counter=0;
+function get_reason(eventEmitter){
+
   return function(data){
+    return new Promise(function(resolve,reject){
+      let http_options=options();
+      http_options.url='http://pubsub2.codeforces.com/ws/'+data.uc+'/'+data.cc+'/'+data.pc+'?_=1472812847834&tag=&time=&eventid='
 
-    let http_options=options();
-    http_options.url='http://pubsub2.codeforces.com/ws/'+data.uc+'/'+data.cc+'/'+data.pc+'?_=1472812847834&tag=&time=&eventid='
-
-    const ws=new WebSocket(http_options.url);
-    var x={};
-    var counter=1;
-    let done=false;
-    ws.on('message', function(data, flags) {
+      const ws=new WebSocket(http_options.url);
+      var x={};
+      let done=false;
+      ws.on('message', function(data, flags) {
 
 
-      // flags.binary will be set if a binary data is received.
-      // flags.masked will be set if the data was masked.
-      let test=JSON.parse(data);
-      var x=test.text;
+        // flags.binary will be set if a binary data is received.
+        // flags.masked will be set if the data was masked.
+        let test=JSON.parse(data);
+        var x=test.text;
+          var x=JSON.parse(x);
+        let reason=x.d[6];
+        let testcase=x.d[8];
+        if(testcase>counter){
+          counter=testcase;
+        if(!done){
+          try{
 
-      var x=JSON.parse(x);
-      if(!done){
-        try{
-          var results=x.d[6];
 
-          //console.log((x.d[4].toString().red+":"+x.d[6].toString()+" on test "+x.d[8].toString()));
-          if(results=='COMPILATION_ERROR' || results=='WRONG_ANSWER'){
-            let reason=x.d[6];
-            let testcase=x.d[8];
 
-            let link='http://www.codeforces.com/contest/'+x.d[2]+'/submission/'+x.d[1];
+            if(reason=='COMPILATION_ERROR' || reason=='WRONG_ANSWER'){
 
-            eventEmitter.emit('result',{success:false,link:link,reason:reason,testcase:testcase});
-            ws.close();
-            done=true;
+
+              let link='http://www.codeforces.com/contest/'+x.d[2]+'/submission/'+x.d[1];
+
+              eventEmitter.emit('result',{success:false,link:link,reason:reason,testcase:testcase});
+              ws.close();
+              done=true;
+            }
+            if(reason=='OK'){
+              let link='http://www.codeforces.com/contest/'+x.d[2]+'/submission/'+x.d[1];
+              eventEmitter.emit('result',{success:true,link:link});
+
+              ws.close();
+              done=true;
+
+            }
+            eventEmitter.emit('live_result',{reason:reason,testcase:testcase});
+
           }
-          if(results=='OK'){
-            let link='http://www.codeforces.com/contest/'+x.d[2]+'/submission/'+x.d[1];
-            eventEmitter.emit('result',{success:true,link:link});
-            ws.close();
-            done=true;
-
+          catch(e){
+            console.log(e);
+            return reject(e);
           }
-
-        }
-        catch(e){
-
         }
       }
-    });
+      });
 
 
 
+    })
   }
 }
 
 
-module.exports=get_results;
+module.exports=get_reason;
